04cf631c124d14ed3cd73cc60b2ff4f531151017
Yanisa Khemdang <cherry0983541@gmail.com>
2019-12-26 10:57:12 +0700

    OverloadMethod

---
+++ src/LAP4/OverloadMethod.java
@@ -0,0 +1,31 @@
+package LAP4;
+
+import java.util.Scanner;
+
+public class OverloadMethod {
+
+    public static void main(String[] args) {
+
+        //Overloading Method : คือ Method ที่มีชื่อเดียวกันแต่มี parameter
+        //และ return value ต่างกัน
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter number 1: ");
+//        System.out.println("Enter number 2:");
+//        int y = sc.nextInt();
+
+        System.out.println(plus(10,20);
+        System.out.println(plus(10.0,20.0));
+        System.out.println(plus(10.0,20));
+
+    } //main
+
+    public static int plus(int x,int y) {
+        return x+y;
+    }
+    public static double plus(double x,double y){
+        return x+y;
+    }
+
+
+} //class
